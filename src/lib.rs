712094f30b8c1f0c1a3029d81454dd1c6106e7ee
Sophia Namaganda <sophia@kali.kali>
2024-04-09 14:56:17 -0500

    leap

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {year} is a leap year")
+   // unimplemented!("true if {year} is a leap year");
+    if year % 100 == 0 && year % 400 == 0 {
+        return true;
+    }
+    if year % 10 == 0 {
+        return false;
+    }
+    if year % 4 == 0 {
+        return true;
+    }
+    return false;
+
 }
